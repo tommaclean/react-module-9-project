524d28c6233e48121b38ffe7d4eb2e0181cc7ccf
tommaclean <tom.j.maclean@gmail.com>
2020-09-08 12:22:47 -0400

    added error handling

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostID: null
+        selectedPostID: null,
+        error: false
     }
 
     componentDidMount () {
@@ -23,8 +24,10 @@ class Blog extends Component {
                     }
                 })
                 this.setState({posts: updatedPosts})
-                console.log(this.state)
             })
+                    .catch(error => {
+                         this.setState({error: true})
+                    })
     }
 
     postSelectedHandler = (id) => {
@@ -32,13 +35,16 @@ class Blog extends Component {
         console.log(this.state.selectedPostID)
     }
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}/>
-        })
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}/>
+            })
+        }
         return (
             <div>
                 <section className="Posts">
